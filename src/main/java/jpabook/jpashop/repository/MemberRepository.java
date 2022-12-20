c9298ae02f2e2aaac19ba7c11ea52bb6096941a2
Seonho Kim <sunhowold@naver.com>
2022-12-20 18:34:39 +0900

    회원 도메인 개발 - 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,18 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // Spring Boot JPA가 @Autowired를 지원 -> @PersistContext -> @AutoWired -> @RequiredArgsConstructor
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // // @Transactional(readOnly = true)가 아니라 @Transac
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() { // @Transactional(readOnly = true) 적용됨
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) { // @Transactional(readOnly = true) 적용됨
+        return memberRepository.findOne(memberId);
+    }
+}
