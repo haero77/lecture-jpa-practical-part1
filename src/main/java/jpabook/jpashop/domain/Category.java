5274d54a368781be0e263e2a015a1040a6cb9421
Seonho Kim <sunhowold@naver.com>
2022-12-20 15:38:45 +0900

    도메인 분석 설계 - 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Parent;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    // 중간 테이블 'category_item'이 필요하다.
+    // 객체 끼리는 다대다 매핑이 가능하지만,
+    // DB 관점에서는 불가능하기 때문에 다대다 관계를 일대다, 다대일 관계로 풀어주기 위한 매핑용 테이블이 필요하다.
+    // 확장이 불가능하므로 실무에서는 사용하지 않는다.
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    // 계층구조 만들기
+    @ManyToOne // 하나의 부모에 여러 자식
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id") // FK 'member_id'
+    private Member member;
+
+    @OneToMany(mappedBy = "order") // OrderItem.order
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    /*
+    * 보통 Delivery보다 Order를 우선적으로 사용하게 되므로, Order 쪽에 FK를 두었다.
+    * 따라서 Order가 연관관계 주인.
+    * @JoinColumn: 이 필드가 주인이다.
+     */
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus status;
+}
